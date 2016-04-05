89b0897c59c1c4e22e22d3b7d468f12d31c1ed01
David Boyd <John.Boyd3@dc.gov>
2016-04-05 17:03:13 +0000

    Using SQLite3

--- README.md
+++
@@ -1,2 +0,0 @@
-# hbx-mobile-server
-Server code required by the new native iOS &amp; Android mobile apps for dchealthlink.com
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,2 @@
-class User
-  include Mongoid::Document
-  field :first_name, type: String
-  field :last_name, type: String
-  field :email, type: String
-  field :password, type: String
-  field :password_confirmation, type: String
-  field :temp_password, type: String    
+class User < ActiveRecord::Base
 end
