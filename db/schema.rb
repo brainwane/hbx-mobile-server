2dc3aeb0caf895dced9c393942ed65589a910caf
Benjamin Rosenbaum <info@benjaminrosenbaum.com>
2016-04-06 15:17:00 -0400

    removed unneeded User and Article models

--- db/schema.rb
+++ db/schema.rb
@@ -11,27 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405172310) do
-
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.text     "device_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "visits", force: :cascade do |t|
-    t.string   "email"
-    t.string   "device_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
