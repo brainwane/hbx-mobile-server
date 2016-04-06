3b5103a94d7ec9b395b66a2416e63d4dc62ea530
Benjamin Rosenbaum <info@benjaminrosenbaum.com>
2016-04-06 17:16:58 -0400

    Mocked out connections to IAM and EnrollApp; fail on 'fail' password

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,12 +1,34 @@
 class VisitsController < ApplicationController
 
 	def create
-		print params_for_create
-		render :json => {session_key: "23GHJ678"}
+		email, password, device_id = params_for_create.values_at(:email, :password, :device_id)
+
+		if saml_token = authenticate?(email, password)
+			visit = Visit.where(email: email).first_or_create
+			visit["device_id"] = device_id
+			visit.save
+			if session_key = establish_session(saml_token)
+				render :json => {session_key: session_key}
+			else 
+				render :json => {error: "Cannot establish a valid connection to the Enroll Application"}, :status => 503
+			end
+		else
+			render :json => {error: "Email/password combination not found"}, :status => 403
+		end
 	end
 
 	def params_for_create
 		params.permit(:email, :password, :device_id)
 	end
 
+	def authenticate?(email, password)
+		#TODO: call IAM, authenticate, return SAML or nil for failure
+		#for now, we mock it by failing on a password of "fail"
+		"some_saml" unless password == "fail"
+	end
+
+	def establish_session(saml_token)
+		#TODO: call EnrollApp with SAML, create session key
+		"SESSION_KEY000"
+	end
 end
