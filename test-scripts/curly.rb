92d83d8decd8de0043c95b81fa769aab1d44d384
Benjamin Rosenbaum <info@benjaminrosenbaum.com>
2016-04-26 15:50:51 -0400

    refactored out hostname

--- test-scripts/curly.rb
+++ test-scripts/curly.rb
@@ -2,6 +2,7 @@
 
 require 'open3'
 
+host = 'http://10.36.27.206:3000'
 
 def capture_form(form_url, filename, params='')
 	out, err, status = Open3.capture3("curl -L -v #{params} #{form_url}")
@@ -16,14 +17,14 @@ def capture_form(form_url, filename, params='')
     [cookie, token]
 end
 
-cookie, token = capture_form('http://10.36.27.206:3000/users/sign_in', 'login_form')
+cookie, token = capture_form('#{host}/users/sign_in', 'login_form')
 print "\nGot cookie: #{cookie} \nGot token: #{token} "
 
-cookie, token = capture_form('http://10.36.27.206:3000/users/sign_in', 'do_login', 
+cookie, token = capture_form('#{host}/users/sign_in', 'do_login', 
 	"-d user[email]=frodo@shire.com -d user[password]=Test123! -d authenticity_token=#{token} -H 'Cookie: _session_id=#{cookie}'")
 print "\nGot cookie: #{cookie} \nGot token: #{token} "
 
-cookie, token = capture_form('http://10.36.27.206:3000/employers/employer_profiles/57167b16ea497f05d2000009?tab=home', 'employer_home', "-H 'Cookie: _session_id=#{cookie}'")
+cookie, token = capture_form('#{host}/employers/employer_profiles/57167b16ea497f05d2000009?tab=home', 'employer_home', "-H 'Cookie: _session_id=#{cookie}'")
 print "\nGot cookie: #{cookie} \nGot token: #{token} "
 
 
